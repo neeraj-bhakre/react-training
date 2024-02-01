bfe01343f779b2b75c550fb157d4f317544e82ff
rameshwarkamandar <155446782+rameshwarkamandar@users.noreply.github.com>
2024-02-01 12:02:07 +0530

    Section 7 Completed

---
+++ Section 7/src/Header.js
@@ -0,0 +1,10 @@
+//import logo from './investment-calculator-logo.png';
+
+export default function Header() {
+  return (
+    <header id="header">
+      <img src="investment-calculator-logo.png" alt="Logo showing a money bag" />
+      <h1>Investment Calculator</h1>
+    </header>
+  );
+}
