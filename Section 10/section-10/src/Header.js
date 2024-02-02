58ccbdeceab7e11d865dc0f919337a73efdc8c73
rameshwarkamandar <155446782+rameshwarkamandar@users.noreply.github.com>
2024-02-02 19:02:24 +0530

    Section 10 Completed

---
+++ Section 10/section-10/src/Header.js
@@ -0,0 +1,41 @@
+import { useRef, useContext } from 'react';
+
+import CartModal from './CartModal.js';
+import { CartContext } from './shopping-cart-context.js';
+
+export default function Header() {
+  const modal = useRef();
+  const { items } = useContext(CartContext);
+
+  const cartQuantity = items.length;
+
+  function handleOpenCartClick() {
+    modal.current.open();
+  }
+
+  let modalActions = <button>Close</button>;
+
+  if (cartQuantity > 0) {
+    modalActions = (
+      <>
+        <button>Close</button>
+        <button>Checkout</button>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <CartModal ref={modal} title="Your Cart" actions={modalActions} />
+      <header id="main-header">
+        <div id="main-title">
+          <img src="logo (3).png" alt="Elegant model" />
+          <h1>Elegant Context</h1>
+        </div>
+        <p>
+          <button onClick={handleOpenCartClick}>Cart ({cartQuantity})</button>
+        </p>
+      </header>
+    </>
+  );
+}
