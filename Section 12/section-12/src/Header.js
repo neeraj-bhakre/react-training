2d7e6c0d15c6aa827caabef7c649c0c00c889663
rameshwarkamandar <155446782+rameshwarkamandar@users.noreply.github.com>
2024-02-03 18:07:18 +0530

    Section 12 Completed

---
+++ Section 12/section-12/src/Header.js
@@ -0,0 +1,10 @@
+//import logoImg from './quiz-logo.png';
+
+export default function Header() {
+  return (
+    <header>
+      <img src="quiz-logo.png" alt="Quiz logo" />
+      <h1>ReactQuiz</h1>
+    </header>
+  );
+}
